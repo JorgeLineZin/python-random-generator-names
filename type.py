b79c909848f635fb1185dce7ded06a741f02307a
Jorge "Line" Felipe <58451132+JorgeLineZin@users.noreply.github.com>
2023-11-14 16:48:49 -0300

    Add files via upload

---
+++ type.py
@@ -0,0 +1,8 @@
+#Quem e corno? 1.0
+import random
+
+frase = "é corno"
+aleatoriador = random.randint(1,20)
+
+if aleatoriador >= 10: print("João",frase, aleatoriador)
+else: print("Paulo",frase, aleatoriador)
